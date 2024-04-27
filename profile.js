6bbfc7dbcf5aabf6167a8bc6c5890b10cf013e22
priyanshu kumar <84118213+priyanshu-k1@users.noreply.github.com>
2024-04-27 20:02:56 +0530

    added gif and text animation

--- profile.js
+++ profile.js
@@ -1,6 +1,37 @@
 function letsChat(){
     window.open("https://wa.me/919302771737",'_blank'); 
-  };
+}
 
 
+let arrayOfID=['text1', 'text2', 'text3', 'text4', 'text5', 'text6', 'text7', 'text8', 'text9', 'text10', 'text11', 'text12'];
+var count=0;
+var forward=true
+
+function lightUP() {
+    if(forward==true){
+        var element=document.getElementById(arrayOfID[count]);
+        element.style.color= "white";
+        element.style.filter='drop-shadow(5px 0px 10px  #6554AF)';
+        count++;
+        if(count==12){
+            forward=false
+        }
+        
+    }
+    else{
+        var element=document.getElementById(arrayOfID[count-1]);
+        element.style.color= "#212529";
+        element.style.filter='drop-shadow(0px 0px 0px  #09101a)';
+        count--;
+        if(count==0){
+            forward=true
+        }
+
+    } 
+  setTimeout(lightUP, 700); 
+}
+
+window.onload = function() {
+    lightUP();
+}
 
